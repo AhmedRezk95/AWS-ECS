af47ef1aea7346a30887b770d8715c152a433d79
rizk <arezk95@gmail.com>
2023-01-31 01:29:19 +0200

    Update ECS

--- ecs.tf
+++ ecs.tf
@@ -15,6 +15,13 @@ resource "aws_ecs_cluster" "ecs_cluster" {
 # }
 
 
+data "template_file" "task_definition_template" {
+    template = file("task_definition.json.tpl")
+    vars = {
+      REPOSITORY_URL = replace(aws_ecr_repository.worker.repository_url, "https://", "")
+    }
+}
+
 
 resource "aws_ecs_task_definition" "task_definition" {
   family                = "worker"
