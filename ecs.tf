7c5e8e78169ed95bf709e01a8e909b3567c534c6
rizk <arezk95@gmail.com>
2023-01-31 09:14:20 +0200

    Update ECS

--- ecs.tf
+++ ecs.tf
@@ -8,25 +8,25 @@ resource "aws_ecs_cluster" "ecs_cluster" {
 }
 
 
-# resource "aws_ecs_task_definition" "task_definition" {
-#   # family if required
-#   family                = "worker"
-#   container_definitions = file("./task_definition.json")
+resource "aws_ecs_task_definition" "task_definition" {
+  # family if required
+  family                = "worker"
+  container_definitions = file("./task_definition.json")
+}
+
+
+# data "template_file" "task_definition_template" {
+#     template = file("task_definition.json.tpl")
+#     vars = {
+#       REPOSITORY_URL = replace(aws_ecr_repository.worker.repository_url, "https://", "")
+#     }
 # }
 
 
-data "template_file" "task_definition_template" {
-    template = file("task_definition.json.tpl")
-    vars = {
-      REPOSITORY_URL = replace(aws_ecr_repository.worker.repository_url, "https://", "")
-    }
-}
-
-
-resource "aws_ecs_task_definition" "task_definition" {
-  family                = "worker"
-  container_definitions = data.template_file.task_definition_template.rendered
-}
+# resource "aws_ecs_task_definition" "task_definition" {
+#   family                = "worker"
+#   container_definitions = data.template_file.task_definition_template.rendered
+# }
 
 
 resource "aws_ecs_service" "worker" {
