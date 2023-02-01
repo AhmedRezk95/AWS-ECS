e0f4992460563dc9ecda88152e44b88c21231ac0
Ahmed Rizk <arezk95@gmail.com>
2023-02-01 23:58:24 +0200

    Update repo

--- ecs.tf
+++ ecs.tf
@@ -18,3 +18,14 @@ resource "aws_ecs_service" "worker" {
   desired_count   = 2
 }
 
+resource "aws_ecs_task_set" "example" {
+  service         = aws_ecs_service.worker.id
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.task_definition.arn
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.example.arn
+    container_name   = "mongo"
+    container_port   = 8080
+  }
+}
--- network/outputs.tf
+++ network/outputs.tf
@@ -14,11 +14,3 @@ output "subnet_id_p2" {
     value = aws_subnet.public-2.id
 }
 
-# output "subnet_id_pr1" {
-#     value = aws_subnet.private-1.id
-# }
-
-# output "subnet_id_pr2" {
-#     value = aws_subnet.private-2.id
-# }
-
