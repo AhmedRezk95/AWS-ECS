9bbec098e3e71535a9e9e9beef34276a80aa8e7a
Ahmed Rizk <arezk95@gmail.com>
2023-02-01 23:48:39 +0200

    Update repo

--- ecs.tf
+++ ecs.tf
@@ -1,7 +1,3 @@
-resource "aws_ecr_repository" "ecr" {
-  name = "rizk"
-}
-
 
 resource "aws_ecs_cluster" "ecs_cluster" {
   name = "my-cluster"
@@ -13,20 +9,6 @@ resource "aws_ecs_task_definition" "task_definition" {
   family                = "worker"
   container_definitions = file("./task_definition.json")
 }
-
-
-# data "template_file" "task_definition_template" {
-#     template = file("task_definition.json.tpl")
-#     vars = {
-#       REPOSITORY_URL = replace(aws_ecr_repository.worker.repository_url, "https://", "")
-#     }
-# }
-
-
-# resource "aws_ecs_task_definition" "task_definition" {
-#   family                = "worker"
-#   container_definitions = data.template_file.task_definition_template.rendered
-# }
 
 
 resource "aws_ecs_service" "worker" {
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
 output "mysql_endpoint" {
   value = aws_db_instance.default.endpoint
 }
-
-output "ecr_repository_worker_endpoint" {
-  value = aws_ecr_repository.ecr.repository_url
-}
